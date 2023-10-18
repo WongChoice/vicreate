924af20cb7e4d540f9c85eb2d2e5e2241800e072
andy <ankitdasmaxymax@gmail.com>
2023-10-18 07:15:14 +0530

    update-works

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Use an official Python runtime as a parent image
-FROM python:3.8
+FROM python:3.9-slim-buster
 
 # Set the working directory to /app
 WORKDIR /app
@@ -8,13 +8,15 @@ WORKDIR /app
 COPY . /app
 
 # Install any needed packages specified in requirements.txt
-RUN pip install -r requirements.txt
+RUN pip install --trusted-host pypi.python.org -r requirements.txt
+RUN pip install --upgrade pytube
 
-# Make port 5000 available to the world outside this container
-EXPOSE 5000
+# Make port 80 available to the world outside this container
+EXPOSE 80
 
-# Define environment variable
-ENV NAME World
+# Define environment variables
+ENV FLASK_APP=app.py
+ENV FLASK_ENV=production
 
-# Run app.py when the container launches
-CMD ["python", "app.py"]
+# Use Gunicorn to run the app
+CMD ["gunicorn", "--bind", "0.0.0.0:80", "app:app"]
--- app.py
+++ app.py
@@ -1,40 +1,70 @@
-import os
+# Import necessary modules from Flask and your application
 from flask import Flask, request, render_template
+import os
 from pytube import YouTube
 from moviepy.editor import VideoFileClip
 
-app = Flask(__name)
+# Create a Flask app instance
+app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+# Define the routes and view functions
+@app.route('/')
 def index():
-    if request.method == "POST":
-        youtube_url = request.form["youtube_url"]
-        if youtube_url:
-            try:
-                # Output file paths
-                video_output_path = os.path.abspath("test.mp4")
-                audio_output_path = os.path.abspath("audio.wav")
+    return render_template('index.html')
 
-                # Step 1: Extract the video from the YouTube link
-                yt = YouTube(youtube_url)
-                stream = yt.streams.get_highest_resolution()
-                stream.download(output_path=os.path.dirname(video_output_path), filename=os.path.basename(video_output_path))
+@app.route('/download', methods=['POST'])
+def download():
+    youtube_url = request.form['youtube_url']
 
-                # Step 2: Extract audio from the video
-                video_clip = VideoFileClip(video_output_path)
-                audio_clip = video_clip.audio
-                audio_clip.write_audiofile(audio_output_path, codec="pcm_s16le")
+    # Define paths for the video and audio folders within the app directory
+    app_dir = os.path.dirname(os.path.abspath(__file__))
+    video_folder = os.path.join(app_dir, "video")
+    audio_folder = os.path.join(app_dir, "audio")
 
-                # Clean up - remove the video file
-                # os.remove(video_output_path)
+    # Ensure the video and audio folders exist, create them if not
+    os.makedirs(video_folder, exist_ok=True)
+    os.makedirs(audio_folder, exist_ok=True)
 
-                return f"Audio extracted successfully. <a href='/'>Go back</a>"
-            except Exception as e:
-                return f"An error occurred: {str(e)} <a href='/'>Go back</a>"
+    # Initialize the error_message variable
+    error_message = None
+
+    # Step 1: Extract the video from the YouTube link
+    try:
+        yt = YouTube(youtube_url)
+        stream = yt.streams.filter(progressive=True, file_extension='mp4').first()
+        if stream:
+            video_output_path = os.path.join(video_folder, "test.mp4")
+            stream.download(output_path=video_folder, filename="test.mp4")
+            print("Video downloaded successfully.")
         else:
-            return "Please enter a YouTube URL. <a href='/'>Go back</a>"
+            error_message = "No suitable video stream found."
+    except Exception as e:
+        error_message = f"An error occurred while downloading the video: {str(e)}"
 
-    return render_template("index.html")
+    # Step 2: Extract audio from the video
+    if not error_message:
+        try:
+            video_clip = VideoFileClip(video_output_path)
+            audio_output_path = os.path.join(audio_folder, "audio.wav")
+            audio_clip = video_clip.audio
+            audio_clip.write_audiofile(audio_output_path, codec="pcm_s16le")
+            print("Audio extracted successfully.")
+
+            # Close the video file to release the resources
+            video_clip.reader.close()
+        except Exception as e:
+            error_message = f"An error occurred while extracting audio: {str(e)}"
+
+    # Clean up - remove the video file if there was no error
+    if os.path.exists(video_output_path) and not error_message:
+        #os.remove(video_output_path)
+        print("Video file removed.")
+
+    if error_message:
+        print(error_message)
+        return error_message
+
+    return f"Audio file saved at {audio_output_path}"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=80)
